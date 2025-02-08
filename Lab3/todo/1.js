3d44fbcd00e4c25263e1b1ad183b4b9122a2e036
AzamatIlyas <azamatiliasov59@gmail.com>
2025-02-08 13:55:39 +0500

    ToDo

--- Lab3/todo/1.js
+++ Lab3/todo/1.js
@@ -3,14 +3,19 @@ let task = document.getElementById("input");
 let i = 0;
 let j = "";
 
+function deleteItem(event) {
+    let del = event.target.closest(".items");
+    if (del) del.remove();
+}
+
 function checkTask(event) {
     let checkbox = event.target;
     let element = checkbox.nextElementSibling;
 
     if (checkbox.checked) {
-        element.style.textDecoration = "line-through"; // Зачёркиваем текст
+        element.style.textDecoration = "line-through";
     } else {
-        element.style.textDecoration = "none"; // Убираем зачеркивание
+        element.style.textDecoration = "none";
     }
 }
 
@@ -23,18 +28,35 @@ function clicking(){
 
     document.getElementById("list").appendChild(box);
 
+    let leftPart = document.createElement("div");
+    leftPart.classList.add("left");
+    box.appendChild(leftPart);
+
     let inputButton = document.createElement("input");
     inputButton.type = "checkbox";
     inputButton.classList.add("done");
     inputButton.addEventListener("change", checkTask); // Добавляем обработчик
-    box.appendChild(inputButton);
+    leftPart.appendChild(inputButton);
 
     let taskName = document.createElement("p");
     taskName.innerText = task.value;
-    box.appendChild(taskName);
+    leftPart.appendChild(taskName);
+
+    let rightPart = document.createElement("div");
+    rightPart.classList.add("right");
+    box.appendChild(rightPart);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleting");
+    rightPart.appendChild(deleteButton);
+
+    let trashImg = document.createElement("img");
+    trashImg.classList.add("trash");
+    trashImg.src = "https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color-round-1/254000/15-64.png";
+    deleteButton.appendChild(trashImg);
+    deleteButton.addEventListener("click", deleteItem);
 
     task.value = "";
-
 }
 
 document.getElementById("adding").addEventListener("click", clicking);
